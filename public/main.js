a32777dfcd145763975d531e6b659c8e5ce44b9a
Mike <mike8411251995@yahoo.com.tw>
2017-04-21 16:24:18 +0800

    Working socket.io

--- public/main.js
+++ public/main.js
@@ -2,5 +2,8 @@ console.log("hi");
 const socket = io();
 
 socket.on('new data', (data) => {
-
+    x = data.x;
+    y = data.y;
+    z = data.z;
+    console.log("x: " + x + " y: " + y + " z: " + z);
 });
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static(`${__dirname}/public`));
 
 var tessel = require('tessel');
 var accel = require('accel-mma84').use(tessel.port['A']);
@@ -18,9 +17,19 @@ app.get('/stream', (request, response) => {
 io.on('connection', function(socket) {
     console.log('a user connected');
     // Initialize the accelerometer.
+    socket.emit('new data', {
+        x: 1,
+        y: 2,
+        z: 3,
+    });
     accel.on('ready', function() {
         // Stream accelerometer data
         accel.on('data', function(xyz) {
+            socket.emit('new data', {
+                x: xyz[0].toFixed(2),
+                y: xyz[1].toFixed(2),
+                z: xyz[2].toFixed(2),
+            });
             console.log('x:', xyz[0].toFixed(2),
                 'y:', xyz[1].toFixed(2),
                 'z:', xyz[2].toFixed(2));
