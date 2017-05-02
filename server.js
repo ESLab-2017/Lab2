76939b1d3578871c9db188542e07268629396d50
Mike <mike8411251995@yahoo.com.tw>
2017-05-02 21:21:21 +0800

    Add calibration in server.js

--- server.js
+++ server.js
@@ -14,31 +14,39 @@ app.get('/stream', (request, response) => {
     response.redirect(camera.url);
 });
 
+var calib = [0, 0]; // calibration shift
+
+console.log('Accelerometer is initializing...');
 accel.on('ready', function() {
     // Stream accelerometer data
-    console.log('Accelerometer is ready.');
+    console.log('Accelerometer is ready...');
     io.on('connection', function(socket) {
-        console.log('a user connected');
+        console.log('A user is connected...');
         // Initialize the accelerometer.
-        socket.emit('newdata', {
-            x: 1,
-            y: 2,
-            z: 3,
-        });
+
+        // socket.emit('newdata', {
+        //     // Pseudo-data for debugging
+        //     x: 1,
+        //     y: 2,
+        //     z: 3,
+        // });
 
         accel.on('data', function(xyz) {
             socket.emit('newdata', {
-                x: xyz[0],
-                y: xyz[1],
+                x: xyz[0] - calib[0],
+                y: xyz[1] - calib[1],
                 z: xyz[2],
             });
-            console.log('x:', xyz[0],
-                'y:', xyz[1],
-                'z:', xyz[2]);
+            console.log('x:', xyz[0] - calib[0], 'y:', xyz[1] - calib[1], 'z:', xyz[2]);
         });
 
         accel.on('error', function(err) {
             console.log('Error:', err);
+        });
+
+        socket.on('newcalib', function(newcalib) {
+            calib[0] = newcalib[0];
+            calib[1] = newcalib[1];
         });
     });
 });
